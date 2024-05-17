fa0d6fbbe4c3b8581b40adbbacdd85772768ef15
Allana Freitas <fallana807@gmail.com>
2024-05-17 18:36:29 +0000

    criação dos primeiros estilos

---
+++ src/pages/Home.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const HomeContainer = styled.div`
+    color: red;
+
+    h1{
+        font-size: 50px;
+        background: yellow;
+        
+    }
+`
